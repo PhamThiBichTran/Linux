9d6c0670a101b1fb04e0de8b138823e96078766e
PhamThiBichTran <phamthibichtran.ntu@gmail.com>
2018-04-19 21:24:13 +0700

    Bai tap Python

---
+++ BaiTap_Python/sohoc.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+a= input("a= ")
+b= input("b= ")
+print'%d + %d = %d' %(a,b,a+b)
+print'%d - %d = %d' %(a,b,a-b)
+print'%d / %d = %d' %(a,b,a/b)
+print'%d * %d = %d' %(a,b,a*b)
+print'%d % %d =%.2d'%(a,b,a/b)
---
+++ BaiTap_Python/swap.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+a= input("a= ")
+b= input("b= ")
+t= a
+a=b
+b=a
+print a,b
